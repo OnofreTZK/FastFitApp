f1fd5ed0daeb1247cea45ca6735e398c8c849ab4
Tiago Onofre <taraujonofre@gmail.com>
2022-01-24 15:38:20 -0300

    Fixed home screen

--- lib/app/app_widget.dart
+++ lib/app/app_widget.dart
@@ -5,7 +5,6 @@ class AppWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Slidy',
       theme: ThemeData(primarySwatch: Colors.blue),
     ).modular();
   }
