d215b9988fafcf2da88f7e50de8861dba00c7d6e
Albuquerque <amanda.f.albuquerque@hotmail.com>
2022-02-13 00:19:14 -0300

    continuando pagina de workout

--- lib/app/modules/workout/presenter/workout_page.dart
+++ lib/app/modules/workout/presenter/workout_page.dart
@@ -41,6 +41,77 @@ class _WorkoutPageState extends State<WorkoutPage> {
           ),
         ),
       ),
+      body: Column(
+        mainAxisSize: MainAxisSize.max,
+        children: [
+          Expanded(
+              child: Padding(
+            padding: EdgeInsetsDirectional.fromSTEB(16, 0, 16, 0),
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  Padding(
+                    padding: EdgeInsetsDirectional.fromSTEB(0, 12, 0, 0),
+                    child: Container(
+                      width: MediaQuery.of(context).size.width,
+                      height: 164,
+                      decoration: BoxDecoration(
+                          color: Color(0xFF090F13),
+                          image: DecorationImage(
+                            fit: BoxFit.fitWidth,
+                            image: Image.asset('assets/images/gym2.jpg')
+                                .image,
+                          ),
+                          boxShadow: [
+                            BoxShadow(
+                              blurRadius: 3,
+                              color: Color(0x33000000),
+                              offset: Offset(0, 2),
+                            )
+                          ],
+                          borderRadius: BorderRadius.circular(8)),
+                      child: Container(
+                        width: 100,
+                        height: 100,
+                        decoration: BoxDecoration(
+                          color: Color(0x65090F13),
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                        child: Column(
+                          mainAxisSize: MainAxisSize.max,
+                          children: [
+                            Padding(
+                              padding: EdgeInsetsDirectional.fromSTEB(
+                                  16, 16, 16, 16),
+                              child: Row(
+                                mainAxisSize: MainAxisSize.max,
+                                children: [
+                                  Expanded(
+                                      child: Text(
+                                        'Exercício 1',
+                                      style: GoogleFonts.poppins(
+                                        color: Colors.white,
+                                        fontSize: 30,
+                                        textStyle: Theme.of(context)
+                                            .textTheme
+                                            .headline6),
+                                      ),
+                                    )
+                                ],
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ))
+        ],
+      ),
     );
   }
 }
